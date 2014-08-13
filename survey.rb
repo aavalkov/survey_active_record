aa2b887516c854c58bd7c3bf7f31994bdc1be318
Jeff Stringer & Anna Valkov <student@epicodus.com>
2014-08-13 10:31:32 -0700

    list_surveys to ui

--- survey.rb
+++ survey.rb
@@ -10,10 +10,15 @@ ActiveRecord::Base.establish_connection(development_configuration)
 
 def main_menu
   puts "Press 's' to add a new survey"
+  puts "Press 'l' to list surveys"
+  puts "Press 's' to exit"
   user_choice = gets.chomp
   case user_choice
     when 's' then add_survey
+    when 'l' then list_survey
+    when 'x' then exit
   end
+  main_menu
 end
 
 def add_survey
@@ -21,6 +26,12 @@ def add_survey
   title = gets.chomp
   new_title = Survey.create({:title => title})
   puts "Survey #{new_title.title} added!"
+end
+
+def list_survey
+  puts "All surveys:\n"
+  Survey.all.each { |s| puts s.title}
+  puts "\n"
 end
 
 main_menu
