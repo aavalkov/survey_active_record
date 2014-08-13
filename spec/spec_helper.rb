3c23357484543a4affa927b574ddb7f61bc60030
Jeff Stringer & Anna Valkov <student@epicodus.com>
2014-08-13 10:10:53 -0700

    updated spec_helper

---
+++ spec/spec_helper.rb
@@ -0,0 +1,13 @@
+require 'active_record'
+require 'rspec'
+require 'survey'
+require 'question'
+require 'response'
+
+ActiveRecord::Base.establish_connection(YAML::load(File.open('./db/config.yml'))["test"])
+
+RSpec.configure do |config|
+  config.after(:each) do
+    Survey.all.each { |survey| survey.destroy }
+  end
+end
